72c205dad9d55ad9299a8f8c6ff3ec53fd9ce79f
Richard Martins <richard.sjnd@hotmail.com>
2025-10-07 21:36:43 -0300

    Switch and About Formated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,19 @@
 import { SwitchComponent } from "@/components/SwitchComponent"
+import { Playfair_Display_SC } from "next/font/google";
+
+const playfair = Playfair_Display_SC({
+  subsets: ["latin"], weight: ["400"]
+});
 
 export default function Home() {
   return (
-    <>
-      <header className="flex justify-end">
-        <div>
-          <SwitchComponent />
-        </div>
-
+    <header className="flex items-center justify-end m-16 gap-6">
       <div>
-        ABOUT
+        <SwitchComponent />
       </div>
-      </header >
-    </>
-
+      <div className={`${playfair.className} text-[20px] pb-2 tracking-widest`}>
+        about
+      </div>
+    </header>
   )
 }
