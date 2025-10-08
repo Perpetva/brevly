a0d7388c430ad6bdf7f6f86748b2a76547ec4473
Richard Martins <richard.sjnd@hotmail.com>
2025-10-07 22:01:22 -0300

    Adding light-mode-logo, tittle and description

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,45 @@
 import { SwitchComponent } from "@/components/SwitchComponent"
-import { Playfair_Display_SC } from "next/font/google";
+import { Playfair_Display_SC, Roboto } from "next/font/google";
+import Image from "next/image";
 
 const playfair = Playfair_Display_SC({
   subsets: ["latin"], weight: ["400"]
-});
+})
+
+const roboto = Roboto({
+  subsets: ["latin"], weight: ["100"]
+})
 
 export default function Home() {
   return (
-    <header className="flex items-center justify-end m-16 gap-6">
-      <div>
-        <SwitchComponent />
-      </div>
-      <div className={`${playfair.className} text-[20px] pb-2 tracking-widest`}>
-        about
-      </div>
-    </header>
+    <>
+      <header className="flex items-center justify-end m-16 gap-6">
+        <div>
+          <SwitchComponent />
+        </div>
+        <div className={`${playfair.className} text-[20px] pb-2 tracking-widest`}>
+          about
+        </div>
+      </header>
+
+      <main className="flex justify-center min-h-screen mt-60">
+        <div className="flex flex-col items-center gap-2">
+          <Image
+            src="/images/light-logo.png"
+            alt="lightlogo"
+            width={106}
+            height={51}
+            className=""
+          />
+          <div className={`text-[40px] ${playfair.className} tracking-wide`}>
+            BREVLY
+          </div>
+          <div className={`text-[20px] ${roboto.className} tracking-[0.25em]`}>
+            The perfect size for your link
+          </div>
+        </div>
+
+      </main>
+    </>
   )
 }
